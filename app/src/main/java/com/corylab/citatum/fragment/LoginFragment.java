1b7b8f326e9076d6ffea60c18e9c9f8dc9a246cf
Vadim <malin.vadimka@mail.ru>
2023-04-12 15:08:03 +0300

    Add layouts: - Add login and register - Add lines in strings.xml - Add new color

---
+++ app/src/main/java/com/corylab/citatum/fragment/LoginFragment.java
@@ -0,0 +1,12 @@
+package com.corylab.citatum.fragment;
+
+import androidx.fragment.app.Fragment;
+
+import com.corylab.citatum.R;
+
+public class LoginFragment extends Fragment {
+
+    public LoginFragment() {
+        super(R.layout.login_fragment);
+    }
+}
